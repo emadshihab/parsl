d5334ed855f10580aee1e0881f793115aabe45ff
Ben Clifford <benc@hawaga.org.uk>
2020-04-20 13:02:30 +0000

    Removed unused fields from UDPRadio protocol

--- parsl/monitoring/db_manager.py
+++ parsl/monitoring/db_manager.py
@@ -416,6 +416,9 @@ class DatabaseManager:
                                       'hostname'],
                              messages=reprocessable_first_resource_messages)
 
+    # this function is specialised on queue tag, and reformats the messages expecting
+    # a different format inside each one. that might not be the clearest way to implement
+    # this.
     def _migrate_logs_to_internal(self, logs_queue, queue_tag, kill_event):
         logger.info("Starting processing for queue {}".format(queue_tag))
 
@@ -423,7 +426,7 @@ class DatabaseManager:
             logger.debug("""Checking STOP conditions for {} threads: {}, {}"""
                          .format(queue_tag, kill_event.is_set(), logs_queue.qsize() != 0))
             try:
-                x, addr = logs_queue.get(timeout=0.1)
+                x, addr = logs_queue.get(timeout=0.1)   # addr is unused... could be tidied?
             except queue.Empty:
                 continue
             else:
@@ -433,7 +436,7 @@ class DatabaseManager:
                     else:
                         self.pending_priority_queue.put(x)
                 elif queue_tag == 'resource':
-                    self.pending_resource_queue.put(x[-1])
+                    self.pending_resource_queue.put(x[-1])  # put last element of data (the message) ignoring the other fields (id, time)
                 elif queue_tag == 'node':
                     self.pending_node_queue.put(x[-1])
 
